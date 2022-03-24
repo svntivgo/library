6cc6f6630d919757f5393bc5d08d27e1036e1b81
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-24 00:13:37 -0500

    addBook controller

--- src/main/java/co/com/sofka/library/controllers/BookController.java
+++ src/main/java/co/com/sofka/library/controllers/BookController.java
@@ -1,8 +1,21 @@
 package co.com.sofka.library.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import co.com.sofka.library.models.Book;
+import co.com.sofka.library.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class BookController {
+
+    @Autowired
+    BookService service;
+
+    @PostMapping("/book")
+    @ResponseStatus(HttpStatus.CREATED)
+    private Mono<Book> addBook (@RequestBody Book book) {
+        return service.addBook(book);
+    }
 }
