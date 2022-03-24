d3b733b960909e0fe3321b74ad081556526368b5
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-24 01:49:58 -0500

    method setId created

--- src/main/java/co/com/sofka/library/models/Resource.java
+++ src/main/java/co/com/sofka/library/models/Resource.java
@@ -29,6 +29,10 @@ public class Resource {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
