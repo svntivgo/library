ee64c9085402fbe9c9cc349856554bcbd95ad11b
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-24 14:23:12 -0500

    findByTitle repo

--- src/main/java/co/com/sofka/library/repositories/ResourceRepository.java
+++ src/main/java/co/com/sofka/library/repositories/ResourceRepository.java
@@ -3,7 +3,9 @@ package co.com.sofka.library.repositories;
 import co.com.sofka.library.models.Resource;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface ResourceRepository extends ReactiveMongoRepository<Resource, String> {
+    Flux<Resource> findByTitle(String title);
 }
