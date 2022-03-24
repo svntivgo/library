a5818b3f8e1dcd82be1ccd414cac5b97937c5e09
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-24 00:03:15 -0500

    addBook service

--- src/main/java/co/com/sofka/library/repositories/services/BookService.java
+++ src/main/java/co/com/sofka/library/repositories/services/BookService.java
@@ -1,12 +1,17 @@
 package co.com.sofka.library.repositories.services;
 
+import co.com.sofka.library.models.Book;
 import co.com.sofka.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BookService {
     @Autowired
     BookRepository repository;
 
+    public Mono<Book> addBook (Book book) {
+        return repository.save(book);
+    }
 }
